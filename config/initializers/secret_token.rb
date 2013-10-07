8cdfe426a85d3c6209633acf055f59cf093e3b15
Andrew Voss <andrew@andrewavoss.com>
2013-10-07 19:48:40 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinclone::Application.config.secret_key_base = 'ad0827d5c8a506ae9adde2cafcd6ee1f5067d9945249f168c6333b741eab82eb6fbe6ff3c669417a42966b78129f6d5e2f2ef7055fc04843417f0344adafbe8d'
